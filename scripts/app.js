3e30885a4f87d59557920d30d9289eaa30fd00a6
paritoshgharpure <phgstudy@gmail.com>
2021-04-09 23:22:18 +0530

    replaced function keywords with arrow functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const playerChoice = document.getElementById('player_choice');
 const computerChoice = document.getElementById('computer_choice');
 const resultPlace = document.getElementById('result_place');
-
+const reasonPlace = document.getElementById('reason_place');
 const rockButton = document.getElementById('rock_button');
 const paperButton = document.getElementById('paper_button');
 const scissorsButton = document.getElementById('scissors_button');
@@ -19,7 +19,7 @@ let isGameRunning = false;
 let playerSelection;
 let computerSelection;
 
-const getComputerChoice = function () {
+const getComputerChoice = () => {
   let ran = Math.random();
   if (ran < 0.34) {
     return ROCK;
@@ -30,22 +30,7 @@ const getComputerChoice = function () {
   }
 };
 
-const startMatch = function (pSelection) {
-  computerSelection = getComputerChoice();
-  gameResult = getWinner(playerSelection, computerSelection);
-  if (gameResult === RESULT_DRAW){
-    resultPlace.style.color = "yellow";
-  } else if (gameResult === RESULT_PLAYER_WINS){
-    resultPlace.style.color = "green";
-  } else if (gameResult === RESULT_COMPUTER_WINS){
-    resultPlace.style.color = "red";
-  }
-  playerChoice.textContent = playerSelection;
-  computerChoice.textContent = computerSelection;
-  resultPlace.textContent = gameResult;
-};
-
-const getWinner = function (pChoice, cChoice) {
+const getWinner = (pChoice, cChoice) => {
   if (pChoice === cChoice) {
     return RESULT_DRAW;
   } else if (
@@ -59,17 +44,42 @@ const getWinner = function (pChoice, cChoice) {
   }
 };
 
-rockButton.addEventListener('click', function () {
+const startMatch = pSelection => {
+  computerSelection = getComputerChoice();
+  gameResult = getWinner(playerSelection, computerSelection);
+  if (gameResult === RESULT_DRAW) {
+    resultPlace.style.color = 'yellow';
+  } else if (gameResult === RESULT_PLAYER_WINS) {
+    resultPlace.style.color = 'green';
+  } else if (gameResult === RESULT_COMPUTER_WINS) {
+    resultPlace.style.color = 'red';
+  }
+  playerChoice.textContent = playerSelection;
+  computerChoice.textContent = computerSelection;
+  resultPlace.textContent = gameResult;
+  let message;
+  if (gameResult === RESULT_DRAW) {
+    message = `You and Computer both picked ${playerSelection} => IT'S A DRAW!`;
+  } else if (gameResult === RESULT_PLAYER_WINS) {
+    message = `${playerSelection} beats ${computerSelection} => YOU WIN!!`;
+  } else if (gameResult === RESULT_COMPUTER_WINS) {
+    message = `${computerSelection} beats ${playerSelection} => YOU LOSE!!`;
+  }
+  reasonPlace.textContent = message;
+  isGameRunning = false;
+};
+
+rockButton.addEventListener('click', () => {
   playerSelection = ROCK;
   startMatch(playerSelection);
 });
 
-paperButton.addEventListener('click', function () {
+paperButton.addEventListener('click', () => {
   playerSelection = PAPER;
   startMatch(playerSelection);
 });
 
-scissorsButton.addEventListener('click', function () {
+scissorsButton.addEventListener('click', () => {
   playerSelection = SCISSORS;
   startMatch(playerSelection);
 });
